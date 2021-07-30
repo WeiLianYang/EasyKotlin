1186c232cc67f37b35a14915fd41e437fa36e2e4
WilliamYang <yangchen_0808@163.com>
2021-07-30 14:20:24 +0800

    Add files via upload

---
+++ app/src/main/java/com/william/easykt/ui/adapter/SimpleAdapter.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright WeiLianYang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.william.easykt.ui.adapter
+
+
+import com.william.base_component.adapter.BaseRvAdapter
+import com.william.base_component.adapter.BaseRvViewHolder
+import com.william.easykt.R
+
+/**
+ * author：William
+ * date：4/23/21 10:39 PM
+ * description：简单适配器
+ */
+class SimpleAdapter : BaseRvAdapter<Any>() {
+
+    override val layoutResourceId: Int
+        get() = R.layout.item_simple
+
+    override fun getItemCount(): Int {
+        return 40
+    }
+
+    override fun onBindViewHolder(holder: BaseRvViewHolder, position: Int, bean: Any?) {
+        holder.setText(R.id.text, "position ====> $position")
+    }
+}
