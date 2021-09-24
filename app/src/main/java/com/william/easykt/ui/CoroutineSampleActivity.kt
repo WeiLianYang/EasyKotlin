ea5fd2bae7e79860778237a820c5f9571c2fe075
William <yangchen_0808@163.com>
2021-09-24 22:41:11 +0800

    add coroutines sample

--- app/src/main/java/com/william/easykt/ui/CoroutineSampleActivity.kt
+++ app/src/main/java/com/william/easykt/ui/CoroutineSampleActivity.kt
@@ -73,6 +73,7 @@ class CoroutineSampleActivity : BaseActivity() {
                 9 -> sample9()
                 10 -> sample10()
                 11 -> sample11()
+                12 -> sample12()
                 else -> {
                 }
             }
@@ -350,4 +351,41 @@ private fun sample11() = runBlocking {
         "Done" // 在它运行得到结果之前取消它
     }
     println("Result is $result")
+}
+
+
+var acquired = 0
+
+class Resource {
+    init {
+        acquired++
+    } // Acquire the resource
+
+    fun close() {
+        acquired--
+    } // Release the resource
+}
+
+/**
+ * This example always prints zero. Resources do not leak.
+ */
+fun sample12() {
+    runBlocking {
+        repeat(100_000) { // Launch 100K coroutines
+            launch {
+                var resource: Resource? = null // Not acquired yet
+                try {
+                    withTimeout(60) { // Timeout of 60 ms
+                        delay(50) // Delay for 50 ms
+                        resource = Resource() // Store a resource to the variable if acquired
+                    }
+                    // We can do something else with the resource here
+                } finally {
+                    resource?.close() // Release the resource if it was acquired
+                }
+            }
+        }
+    }
+    // Outside of runBlocking all coroutines have completed
+    println(acquired) // Print the number of resources still acquired
 }
