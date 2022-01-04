a04e06881362c4512f63b065da908479f28ebf87
William <yangchen_0808@163.com>
2022-01-04 15:52:17 +0800

    update jetpack compose demo

---
+++ app/src/main/java/com/william/easykt/data/Message.kt
@@ -0,0 +1,6 @@
+package com.william.easykt.data
+
+/**
+ * 消息模型
+ */
+data class Message(val author: String, val body: String)
--- app/src/main/java/com/william/easykt/ui/ComposeActivity.kt
+++ app/src/main/java/com/william/easykt/ui/ComposeActivity.kt
@@ -1,15 +1,36 @@
 package com.william.easykt.ui
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.william.easykt.R
+import com.william.easykt.data.Message
+import com.william.easykt.data.SampleData
 import com.william.easykt.ui.ui.theme.EasyKotlinTheme
 
+/**
+ * https://developer.android.com/jetpack/compose/tutorial
+ */
 class ComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,11 +38,109 @@ class ComposeActivity : ComponentActivity() {
             EasyKotlinTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    // demo
+                    // Greeting("William")
+
+                    // single message card
+                    /*
+                    MessageCard(
+                        Message(
+                            "Android",
+                            "Hey, take a look at Jetpack Compose, it's great!"
+                        )
+                    )
+                    */
+
+                    // conversation list
+                    Conversation(SampleData.conversationSample)
                 }
             }
         }
     }
+}
+
+@Composable
+fun Conversation(list: List<Message>) {
+    LazyColumn {
+        items(list) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.app_logo),
+            contentDescription = "App logo",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+        // We keep track if the message is expanded or not in this
+        // variable
+        // 可以使用 remember 将本地状态存储在内存中，并跟踪传递给 mutableStateOf 的值的变化
+        // 该值更新时，系统会自动重新绘制使用此状态的可组合项（及其子项）。我们将这一功能称为重组。
+        // import androidx.compose.runtime.getValue
+        // import androidx.compose.runtime.setValue
+        var isExpanded by remember { mutableStateOf(false) }
+
+        // surfaceColor will be updated gradually from one color to the other
+        val surfaceColor: Color by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+
+        // We toggle the isExpanded variable when we click on this Column
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                // surfaceColor color will be changing gradually from primary to surface
+                color = surfaceColor,
+                // animateContentSize will change the Surface size gradually
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
+                )
+            }
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun PreViewConversation() {
+    EasyKotlinTheme {
+        Conversation(SampleData.conversationSample)
+    }
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, showBackground = true, name = "Light Mode")
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true, name = "Dark Mode")
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(
+        msg = Message("William", "Hey, take a look at Jetpack Compose, it's great!")
+    )
 }
 
 @Composable
