ea2290e9f6835f59b4c09fcc5c51cac02a1d0dd0
William <yangchen_0808@163.com>
2022-05-08 15:41:37 +0800

    add lensfacing change function

--- app/src/main/java/com/william/easykt/ui/CameraxActivity.kt
+++ app/src/main/java/com/william/easykt/ui/CameraxActivity.kt
@@ -18,9 +18,13 @@ package com.william.easykt.ui
 
 import android.Manifest
 import android.content.ContentValues
+import android.content.Context
 import android.content.pm.PackageManager
+import android.content.res.Configuration
+import android.hardware.display.DisplayManager
 import android.os.Build
 import android.provider.MediaStore
+import android.view.Surface
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.video.*
@@ -60,10 +64,26 @@ class CameraxActivity : BaseActivity() {
 
     private lateinit var cameraExecutor: ExecutorService
 
+    private var displayId = -1
+    private var lensFacing = CameraSelector.LENS_FACING_BACK
+    private var cameraProvider: ProcessCameraProvider? = null
+
+    private val displayManager by lazy {
+        getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
+    }
+
     override fun initAction() {
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        viewBinding.viewFinder.post {
+            displayId = viewBinding.viewFinder.display.displayId
+        }
+        // 每次设备的方向发生变化时，更新相机用例
+        displayManager.registerDisplayListener(displayListener, null)
+
         // Request permissions
         if (allPermissionsGranted()) {
-            startCamera()
+            setUpCamera()
         } else {
             ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE)
         }
@@ -71,7 +91,15 @@ class CameraxActivity : BaseActivity() {
         viewBinding.btnCaptureImage.setOnClickListener { takePhoto() }
         viewBinding.btnCaptureVideo.setOnClickListener { captureVideo() }
 
-        cameraExecutor = Executors.newSingleThreadExecutor()
+        viewBinding.btnSwitchLensFacing.setOnClickListener {
+            lensFacing = if (CameraSelector.LENS_FACING_FRONT == lensFacing) {
+                CameraSelector.LENS_FACING_BACK
+            } else {
+                CameraSelector.LENS_FACING_FRONT
+            }
+            // 重新绑定相机用例
+            bindCameraUseCases()
+        }
     }
 
     override fun initData() {
@@ -94,8 +122,14 @@ class CameraxActivity : BaseActivity() {
             }
         }
 
+        val metadata = ImageCapture.Metadata().apply {
+            // 使用前置摄像头时镜像
+            isReversedHorizontal = lensFacing == CameraSelector.LENS_FACING_FRONT
+        }
+
         val outputOptions = ImageCapture.OutputFileOptions
             .Builder(contentResolver, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+//            .setMetadata(metadata)
             .build()
 
         // 拍照后回调事件
@@ -191,58 +225,146 @@ class CameraxActivity : BaseActivity() {
     /**
      * 启动相机
      */
-    private fun startCamera() {
+    private fun setUpCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
         cameraProviderFuture.addListener({
             // 绑定摄像机的生命周期
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            cameraProvider = cameraProviderFuture.get()
 
-            // 预览 相机取景器
-            val preview = Preview.Builder()
-                .build()
-                .also {
-                    it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
-                }
-
-            // 录制音视频
-            val recorder = Recorder.Builder()
-                .setQualitySelector(QualitySelector.from(Quality.HIGHEST))
-                .build()
-
-            videoCapture = VideoCapture.withOutput(recorder)
-
-            imageCapture = ImageCapture.Builder().build()
-
-            // 创建图片分析
-            val imageAnalyzer = ImageAnalysis.Builder()
-                .build()
-                .also {
-                    it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { l ->
-                        "Average luminosity: $l".logD()
-                    })
-                }
-
-            // 默认选择后置摄像头
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-            try {
-                // 在重新绑定之前取消绑定用例
-                cameraProvider.unbindAll()
-
-                // 将用例绑定到相机
-                // Preview + VideoCapture + ImageCapture： LIMITED设备及以上。
-                // Preview + VideoCapture + ImageAnalysis：（ LEVEL_3最高）设备添加到 Android 7(N)。
-                // 预览 + VideoCapture + ImageAnalysis + ImageCapture：不支持。
-                cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview, imageCapture, videoCapture
-                )
-
-            } catch (e: Exception) {
-                "Use case binding failed, $e".logE()
+            // 镜头朝向
+            lensFacing = when {
+                hasBackCamera() -> CameraSelector.LENS_FACING_BACK
+                hasFrontCamera() -> CameraSelector.LENS_FACING_FRONT
+                else -> throw IllegalStateException("Back and front camera are unavailable")
             }
 
+            bindCameraUseCases()
         }, ContextCompat.getMainExecutor(this))
+    }
+
+    /**
+     * 绑定相机用例
+     */
+    private fun bindCameraUseCases() {
+        val rotation = viewBinding.viewFinder.display?.rotation ?: Surface.ROTATION_0
+
+        val cameraProvider =
+            cameraProvider ?: throw IllegalStateException("Camera initialization failed.")
+
+        // 预览 相机取景器
+        val preview = Preview.Builder()
+            .setTargetRotation(rotation)
+            .build()
+            .also {
+                it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
+            }
+
+        // 录制音视频
+        val recorder = Recorder.Builder()
+            .setQualitySelector(QualitySelector.from(Quality.HIGHEST))
+            .build()
+
+        videoCapture = VideoCapture.withOutput(recorder)
+
+        imageCapture = ImageCapture.Builder()
+            .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+            .setTargetRotation(rotation)
+            .build()
+
+        // 创建图片分析
+        /*
+        val imageAnalyzer = ImageAnalysis.Builder()
+            .setTargetRotation(rotation)
+            .build()
+            .also {
+                it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { l ->
+                    "Average luminosity: $l".logD()
+                })
+            }
+            */
+
+        // 构建摄像头
+        val cameraSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
+
+        try {
+            // 在重新绑定之前取消绑定用例
+            cameraProvider.unbindAll()
+
+            // 将用例绑定到相机
+            // Preview + VideoCapture + ImageCapture： LIMITED设备及以上。
+            // Preview + VideoCapture + ImageAnalysis：（ LEVEL_3最高）设备添加到 Android 7(N)。
+            // 预览 + VideoCapture + ImageAnalysis + ImageCapture：不支持。
+            val camera = cameraProvider.bindToLifecycle(
+                this, cameraSelector, preview, imageCapture, videoCapture
+            )
+            observeCameraState(camera.cameraInfo)
+        } catch (e: Exception) {
+            "Use case binding failed, $e".logE()
+        }
+    }
+
+    /**
+     * 如果设备有可用的后置摄像头，则返回 true
+     */
+    private fun hasBackCamera(): Boolean {
+        return cameraProvider?.hasCamera(CameraSelector.DEFAULT_BACK_CAMERA) ?: false
+    }
+
+    /**
+     * 如果设备有可用的前置摄像头，则返回 true
+     */
+    private fun hasFrontCamera(): Boolean {
+        return cameraProvider?.hasCamera(CameraSelector.DEFAULT_FRONT_CAMERA) ?: false
+    }
+
+    private fun observeCameraState(cameraInfo: CameraInfo) {
+        cameraInfo.cameraState.observe(this) { cameraState ->
+
+            when (cameraState.type) {
+                CameraState.Type.PENDING_OPEN -> {
+                    "CameraState: Pending Open".logD()
+                }
+                CameraState.Type.OPENING -> {
+                    "CameraState: Opening".logD()
+                }
+                CameraState.Type.OPEN -> {
+                    "CameraState: Open".logD()
+                }
+                CameraState.Type.CLOSING -> {
+                    "CameraState: Closing".logD()
+                }
+                CameraState.Type.CLOSED -> {
+                    "CameraState: Closed".logD()
+                }
+            }
+
+            cameraState.error?.let { error ->
+                when (error.code) {
+                    CameraState.ERROR_STREAM_CONFIG -> {
+                        "Stream config error".logD()
+                    }
+                    CameraState.ERROR_CAMERA_IN_USE -> {
+                        "Camera in use".logD()
+                    }
+                    CameraState.ERROR_MAX_CAMERAS_IN_USE -> {
+                        "Max cameras in use".logD()
+                    }
+                    CameraState.ERROR_OTHER_RECOVERABLE_ERROR -> {
+                        "Other recoverable error".logD()
+                    }
+                    CameraState.ERROR_CAMERA_DISABLED -> {
+                        "Camera disabled".logD()
+                    }
+                    CameraState.ERROR_CAMERA_FATAL_ERROR -> {
+                        "Fatal error".logD()
+                    }
+                    CameraState.ERROR_DO_NOT_DISTURB_MODE_ENABLED -> {
+                        "Do not disturb mode enabled".logD()
+                    }
+                }
+            }
+        }
     }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
@@ -255,7 +377,7 @@ class CameraxActivity : BaseActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == REQUEST_CODE) {
             if (allPermissionsGranted()) {
-                startCamera()
+                setUpCamera()
             } else {
                 showSnackbar(viewBinding.root, "Permissions not granted by the user.")
             }
@@ -265,6 +387,7 @@ class CameraxActivity : BaseActivity() {
     override fun onDestroy() {
         super.onDestroy()
         cameraExecutor.shutdown()
+        displayManager.unregisterDisplayListener(displayListener)
     }
 
     companion object {
@@ -276,6 +399,32 @@ class CameraxActivity : BaseActivity() {
                     add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 }
             }.toTypedArray()
+    }
+
+    /**
+     * 在配置更改时膨胀相机控件并手动更新 UI，以避免从视图层次结构中删除和重新添加取景器；
+     * 这在支持它的设备上提供了无缝的旋转过渡。注意：从 Android 8 开始支持该标志，但对于运行 Android 9 或更低版本的设备，屏幕上仍有小闪烁。
+     */
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        bindCameraUseCases()
+    }
+
+    /**
+     * 对于不触发配置更改的方向更改，我们需要一个显示侦听器，
+     * 例如，如果我们选择覆盖清单中的配置更改或 180 度方向更改。
+     */
+    private val displayListener = object : DisplayManager.DisplayListener {
+        override fun onDisplayAdded(displayId: Int) {}
+        override fun onDisplayRemoved(displayId: Int) {}
+
+        override fun onDisplayChanged(displayId: Int) = viewBinding.root.let { view ->
+            if (displayId == this@CameraxActivity.displayId) {
+                "Rotation changed: ${view.display.rotation}".logD()
+                imageCapture?.targetRotation = view.display.rotation
+            }
+        }
     }
 
     /**
