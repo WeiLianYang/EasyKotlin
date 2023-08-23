f6280c102555262ee0f4893c73119d5fc4809eef
William <yangchen_0808@163.com>
2023-08-23 19:19:31 +0800

    feat: optimize coil image loader

--- app/src/main/java/com/william/easykt/di/NetworkModule.kt
+++ app/src/main/java/com/william/easykt/di/NetworkModule.kt
@@ -7,6 +7,8 @@ import coil.decode.GifDecoder
 import coil.decode.ImageDecoderDecoder
 import coil.decode.SvgDecoder
 import coil.decode.VideoFrameDecoder
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
 import coil.util.DebugLogger
 import com.william.easykt.BuildConfig
 import dagger.Module
@@ -26,6 +28,7 @@ object NetworkModule {
     @Provides
     @Singleton
     fun okHttpCallFactory(): Call.Factory = OkHttpClient.Builder()
+//        .dispatcher(Dispatcher().apply { maxRequestsPerHost = maxRequests })
         .addInterceptor(
             HttpLoggingInterceptor()
                 .apply {
@@ -49,23 +52,45 @@ object NetworkModule {
         okHttpCallFactory: Call.Factory,
         @ApplicationContext application: Context,
     ): ImageLoader = ImageLoader.Builder(application)
-        .callFactory(okHttpCallFactory)
         .components {
-            add(SvgDecoder.Factory())
-            add(VideoFrameDecoder.Factory())
+            // GIFs
             if (Build.VERSION.SDK_INT >= 28) {
                 add(ImageDecoderDecoder.Factory())
             } else {
                 add(GifDecoder.Factory())
             }
+            // SVGs
+            add(SvgDecoder.Factory())
+            // Video frames
+            add(VideoFrameDecoder.Factory())
         }
-        // Assume most content images are versioned urls
-        // but some problematic images are fetching each time
+        .memoryCache {
+            MemoryCache.Builder(application)
+                // Set the max size to 25% of the app's available memory.
+                .maxSizePercent(0.25)
+                .build()
+        }
+        .diskCache {
+            DiskCache.Builder()
+                .directory(application.filesDir.resolve("coil_image_cache"))
+                .maxSizeBytes(512L * 1024 * 1024) // 512MB
+                .build()
+        }
+        .callFactory(okHttpCallFactory)
+//        .okHttpClient {
+//            // Don't limit concurrent network requests by host.
+//            val dispatcher = Dispatcher().apply { maxRequestsPerHost = maxRequests }
+//
+//            // Lazily create the OkHttpClient that is used for network operations.
+//            OkHttpClient.Builder()
+//                .dispatcher(dispatcher)
+//                .build()
+//        }
+        // Show a short crossfade when loading images asynchronously.
+        .crossfade(true)
+        // Ignore the network cache headers and always read from/write to the disk cache.
         .respectCacheHeaders(false)
-        .apply {
-            if (BuildConfig.DEBUG) {
-                logger(DebugLogger())
-            }
-        }
+        // Enable logging to the standard Android log if this is a debug build.
+        .apply { if (BuildConfig.DEBUG) logger(DebugLogger()) }
         .build()
 }
